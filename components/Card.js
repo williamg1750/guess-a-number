cf2d4c9379dcf0a980f9837cbcc0c3e29ec2a538
william <william.guan12@gmail.com>
2022-01-05 18:45:33 -0500

    user can submit a number with valid constraints

--- components/Card.js
+++ components/Card.js
@@ -24,4 +24,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default StartGameScreen;
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,19 +1,89 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import Card from '../components/Card';
+import Colors from '../Constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState(false);
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    //check to see if the value is between 0 - 99 or not a num then you return and not set states that shouldnt be set
+    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue('');
+    //doesnt matter what order the hook sets are in they are batched togather in on render cycle
+  };
+
+  let confirmedOutput;
+  //logic to show feedback which shows JSX code
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectedNumber} </Text>;
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start A New Game</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <Button title='Reset' onPress={() => {}} />
-          <Button title='Confirm' onPress={() => {}} />
-        </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start A New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize='none'
+            autoCorrect={false}
+            keyboardType='number-pad'
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title='Reset'
+                onPress={resetInputHandler}
+                color={Colors.accent}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title='Confirm'
+                onPress={confirmInputHandler}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+        {confirmedOutput}
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -31,22 +101,19 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
   },
 });
 
